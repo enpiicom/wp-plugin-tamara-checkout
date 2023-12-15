2793029dceef54ae7497a54de858fcad42023252
Loi Le, Thi Kim <loi.le@enpii.com>
2023-12-15 10:07:52 +0700

    Update phpcs for Register_Tamara_Webhook_Job

--- src/App/Jobs/Register_Tamara_Webhook_Job.php
+++ src/App/Jobs/Register_Tamara_Webhook_Job.php
@@ -30,9 +30,9 @@ class Register_Tamara_Webhook_Job extends Base_Job implements ShouldQueue {
 		$webhook_id = $gateway_settings->tamara_webhook_id ?? null;
 		$tamara_client_service->reinit_tamara_client( $gateway_settings->api_token, $gateway_settings->api_url );
 
-		if (empty($webhook_id) || !is_string($webhook_id)) {
+		if ( empty( $webhook_id ) || ! is_string( $webhook_id ) ) {
 			$tamara_api_request = new RegisterWebhookRequest(
-				wp_app_route_wp_url('tamara-webhook'),
+				wp_app_route_wp_url( 'tamara-webhook' ),
 				[
 					'order_approved',
 					'order_declined',
@@ -46,13 +46,12 @@ class Register_Tamara_Webhook_Job extends Base_Job implements ShouldQueue {
 
 			try {
 				$tamara_api_response = $tamara_client_service->get_api_client()->registerWebhook( $tamara_api_request );
-				dev_error_log( $tamara_api_response );
-				if ($tamara_api_response->isSuccess()) {
+				if ( $tamara_api_response->isSuccess() ) {
 					$this->update_webhook_id_to_options(
 						$tamara_gateway_service,
 						$tamara_api_response->getWebhookId()
 					);
-				} elseif ('webhook_already_registered' === $tamara_api_response->getErrors()[0]['error_code']) {
+				} elseif ( $tamara_api_response->getErrors()[0]['error_code'] === 'webhook_already_registered' ) {
 					$this->update_webhook_id_to_options(
 						$tamara_gateway_service,
 						$tamara_api_response->getErrors()[0]['data']['webhook_id']
@@ -60,14 +59,15 @@ class Register_Tamara_Webhook_Job extends Base_Job implements ShouldQueue {
 				} else {
 					throw new Exception(
 						$tamara_gateway_service->_t( $tamara_api_response->getMessage() ),
-						$tamara_api_response->getStatusCode());
+						$tamara_api_response->getStatusCode()
+					);
 				}
-			} catch (Exception $RegisterWebhookException) {
+			} catch ( Exception $RegisterWebhookException ) {
 				throw new Exception(
 					sprintf(
 						"Tamara Service timeout or disconnected.\nError message: ' %s'.\nTrace: %s",
-						$RegisterWebhookException->getMessage(),
-						$RegisterWebhookException->getTraceAsString()
+						esc_html( $RegisterWebhookException->getMessage() ),
+						esc_html( $RegisterWebhookException->getTraceAsString() )
 					)
 				);
 			}
@@ -78,7 +78,7 @@ class Register_Tamara_Webhook_Job extends Base_Job implements ShouldQueue {
 	 * @param  \Tamara_Checkout\App\WP\Payment_Gateways\Tamara_WC_Payment_Gateway  $tamara_gateway_service
 	 * @param  null  $webhook_id
 	 */
-	protected function update_webhook_id_to_options ( Tamara_WC_Payment_Gateway $tamara_gateway_service, $webhook_id = null ): void {
+	protected function update_webhook_id_to_options( Tamara_WC_Payment_Gateway $tamara_gateway_service, $webhook_id = null ): void {
 		$tamara_gateway_service->settings['tamara_webhook_id'] = $webhook_id;
 		$tamara_gateway_service->update_settings_to_options();
 	}
