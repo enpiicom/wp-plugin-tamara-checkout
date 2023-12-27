9cf0c67bb7762ca2e604c747f17b38ead6964202
Loi Le, Thi Kim <loi.le@enpii.com>
2023-12-27 16:08:56 +0700

    Add prepare order to capture

--- src/App/Jobs/Process_Tamara_Capture_Job.php
+++ src/App/Jobs/Process_Tamara_Capture_Job.php
@@ -7,10 +7,13 @@ namespace Tamara_Checkout\App\Jobs;
 use Enpii_Base\Foundation\Bus\Dispatchable_Trait;
 use Enpii_Base\Foundation\Shared\Base_Job;
 use Tamara_Checkout\App\Exceptions\Tamara_Exception;
+use Tamara_Checkout\App\Queries\Build_Tamara_Create_Checkout_Request_Query;
 use Tamara_Checkout\App\Support\Helpers\General_Helper;
 use Tamara_Checkout\App\Support\Helpers\Tamara_Order_Helper;
 use Tamara_Checkout\App\Support\Traits\Trans_Trait;
 use Tamara_Checkout\App\WP\Tamara_Checkout_WP_Plugin;
+use Tamara_Checkout\Deps\Tamara\Model\Money;
+use Tamara_Checkout\Deps\Tamara\Model\Payment\Capture;
 use Tamara_Checkout\Deps\Tamara\Request\Order\GetOrderByReferenceIdRequest;
 use Tamara_Checkout\Deps\Tamara\Request\Payment\CaptureRequest;
 
@@ -53,8 +56,9 @@ class Process_Tamara_Capture_Job extends Base_Job {
 		if ($tamara_order_status === Tamara_Checkout_WP_Plugin::TAMARA_FULLY_CAPTURED_STATUS) {
 			$this->process_capture_successfully();
 		} else {
+			$capture_request = $this->build_capture_request();
 			$tamara_client_response = Tamara_Checkout_WP_Plugin::wp_app_instance()->get_tamara_client_service()->capture_order(
-				new CaptureRequest( $this->prepare_wc_order_to_capture() )
+				new CaptureRequest( $capture_request )
 			);
 		}
 	}
@@ -64,14 +68,25 @@ class Process_Tamara_Capture_Job extends Base_Job {
 		return true;
 	}
 
-	protected function prepare_wc_order_to_capture() {
-		$wc_order = $this->wc_order;
-		$wc_order_total = General_Helper::format_tamara_number(($wc_order->get_total()), $wc_order->get_currency());
-		$wc_shipping_total = General_Helper::format_tamara_number(($wc_order->get_shipping_total()), $wc_order->get_currency());
-		$wc_tax_total = General_Helper::format_tamara_number(($wc_order->get_total_tax()), $wc_order->get_currency());
-		$wc_discount_total = General_Helper::format_tamara_number(($wc_order->get_discount_total()), $wc_order->get_currency());
-		$wc_order_shipping_info = Tamara_Order_Helper::get_tamara_shipping_info();
-		$wc_order_items = $this->populateTamaraOrderItems($wcOrder);
+	protected function build_capture_request(): CaptureRequest {
 		$tamara_order_id = $this->tamara_order_id;
+		$wc_order = $this->wc_order;
+		$wc_order_total = new Money(General_Helper::format_tamara_number($wc_order->get_total()), $wc_order->get_currency());
+		$wc_shipping_total =  new Money(General_Helper::format_tamara_number($wc_order->get_shipping_total()), $wc_order->get_currency());
+		$wc_tax_total = new Money(General_Helper::format_tamara_number($wc_order->get_total_tax()), $wc_order->get_currency());
+		$wc_discount_total = new Money(General_Helper::format_tamara_number($wc_order->get_discount_total()), $wc_order->get_currency());
+		// Todo: Add build order items
+		$wc_order_items = populate_tamara_order_items($wc_order);
+		$wc_order_shipping_info = Tamara_Order_Helper::get_tamara_shipping_info();
+		$capture = new Capture(
+			$tamara_order_id,
+			$wc_order_total,
+			$wc_shipping_total,
+			$wc_tax_total,
+			$wc_discount_total,
+			$wc_order_items,
+			$wc_order_shipping_info
+		);
+		return new CaptureRequest($capture);
 	}
 }
