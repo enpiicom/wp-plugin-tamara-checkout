dcc680aac8c7784146db4023fb0ac8ab0d0d30c4
Loi Le, Thi Kim <loi.le@enpii.com>
2023-12-27 17:52:49 +0700

    Refactor mainjs file

--- public-assets/src/js/main.js
+++ public-assets/src/js/main.js
@@ -1,26 +1,29 @@
 /* Main JS */
 'use strict';
 
-// Tamara Widgets call
 (function ($) {
-
-	// Set new data instalment plan and price to PDP Widget
-	function set_new_PDP_widget_value(new_value) {
-		console.log(new_value)
-		setTimeout(() => {
-			let tamara_widget = $('tamara-widget');
-			// Assigning new data
-			tamara_widget.attr('amount', new_value);
-			window.TamaraWidgetV2.refresh();
-		}, 1000);
-	}
-
-	// Trigger when a new variation is selected on FE
 	$(document).ready(function () {
+		// Set new price value to PDP Widget
+		function set_new_PDP_widget_value(new_value) {
+			console.log(new_value)
+			setTimeout(() => {
+				let tamara_widget = $('tamara-widget');
+				// Assigning new data
+				tamara_widget.attr('amount', new_value);
+				window.TamaraWidgetV2.refresh();
+			}, 1000);
+		}
+
+		// Trigger when a new variation is selected on FE
 		$('.variations_form').each(function () {
 			$(this).on('found_variation', function (event, variation) {
 				set_new_PDP_widget_value(variation.display_price);
 			});
+		});
+
+		// Trigger ajax call whenever phone number is updated on checkout
+		$('input[name=billing_phone]').change(function () {
+			$('body').trigger('update_checkout');
 		});
 	});
 
