a5d95d5d900635c7e8f3f79b4895df0b104b96b6
Trac Nguyen <npbtrac@yahoo.com>
2024-01-31 15:23:36 +0700

    Update the Enpii Base

--- src/App/Jobs/Authorise_Tamara_Stuck_Approved_Orders_Job.php
+++ src/App/Jobs/Authorise_Tamara_Stuck_Approved_Orders_Job.php
@@ -49,7 +49,7 @@ class Authorise_Tamara_Stuck_Approved_Orders_Job extends Base_Job implements Sho
 	}
 
 	public function handle() {
-		dev_error_log('Authorise_Tamara_Stuck_Approved_Orders_Job');
+		dev_error_log( 'Authorise_Tamara_Stuck_Approved_Orders_Job' );
 		$this->before_handle();
 
 		$site_id = (int) get_current_blog_id();
@@ -58,69 +58,69 @@ class Authorise_Tamara_Stuck_Approved_Orders_Job extends Base_Job implements Sho
 		$wc_status_pending = 'wc-pending';
 		$wc_status_payment_authorised_failed = $this->tamara_gateway()->get_settings()->order_status_when_tamara_authorisation_fails;
 		// $pending_orders_query = WC_Order_Model::site( $site_id )->where(
-		// 	[
-		// 		[ 'type', 'shop_order' ],
-		// 		[ 'date_created_gmt', '>=', now()->subDays( 30 )->startOfDay() ],
-		// 		[ 'payment_method', 'LIKE', $this->default_payment_gateway_id() . '%' ],
-		// 	]
+		//  [
+		//      [ 'type', 'shop_order' ],
+		//      [ 'date_created_gmt', '>=', now()->subDays( 30 )->startOfDay() ],
+		//      [ 'payment_method', 'LIKE', $this->default_payment_gateway_id() . '%' ],
+		//  ]
 		// )
 		// ->where(
-		// 	function ( $query ) use ( $wc_status_pending, $wc_status_payment_authorised_failed ) {
-		// 		/** @var \Illuminate\Database\Eloquent\Builder $query */
-		// 		$query->where( 'status', $wc_status_pending )
-		// 			->orWhere( 'status', $wc_status_payment_authorised_failed );
-		// 	}
+		//  function ( $query ) use ( $wc_status_pending, $wc_status_payment_authorised_failed ) {
+		//      /** @var \Illuminate\Database\Eloquent\Builder $query */
+		//      $query->where( 'status', $wc_status_pending )
+		//          ->orWhere( 'status', $wc_status_payment_authorised_failed );
+		//  }
 		// )
 		// ->orderBy( 'date_created_gmt', 'asc' )
 		// ->limit( 7 );
 
 		$wc_order_args = [
-            'fields' => 'ids',
-            'post_type' => 'shop_order',
-            'post_status' => [ $wc_status_pending, $wc_status_payment_authorised_failed ],
-            'date_query' => [
-                'after' => now()->subDays( 30 )->startOfDay(),
-                'inclusive' => true,
-            ],
-            'meta_query' => [
-                'relation' => 'AND',
-                [
-                    'key' => '_tamara_checkout_session_id',
-                    'compare' => 'EXISTS',
-                ],
-                [
-                    'key' => '_tamara_order_id',
-                    'compare' => 'NOT EXISTS',
-                ],
-            ],
+			'fields' => 'ids',
+			'post_type' => 'shop_order',
+			'post_status' => [ $wc_status_pending, $wc_status_payment_authorised_failed ],
+			'date_query' => [
+				'after' => now()->subDays( 30 )->startOfDay(),
+				'inclusive' => true,
+			],
+			'meta_query' => [
+				'relation' => 'AND',
+				[
+					'key' => '_tamara_checkout_session_id',
+					'compare' => 'EXISTS',
+				],
+				[
+					'key' => '_tamara_order_id',
+					'compare' => 'NOT EXISTS',
+				],
+			],
 			'orderby' => [
 				'post_date ASC',
 			],
-        ];
+		];
 
-        $wc_orders_query = new \WP_Query($wc_order_args);
+		$wc_orders_query = new \WP_Query( $wc_order_args );
 
-        $wc_order_ids = $wc_orders_query->posts;
-		dev_error_log('wc_order_ids', $wc_order_ids );
+		$wc_order_ids = $wc_orders_query->posts;
+		// dev_error_log( 'wc_order_ids', $wc_order_ids );
 
-        foreach ($wc_order_ids as $wc_order_id) {
-            // Authorise_Tamara_Order_If_Possible_Job::dispatchSync(
-			// 	[
-			// 		'wc_order_id' => $wc_order_id,
-			// 	]
+		foreach ( $wc_order_ids as $wc_order_id ) {
+			// Authorise_Tamara_Order_If_Possible_Job::dispatchSync(
+			//  [
+			//      'wc_order_id' => $wc_order_id,
+			//  ]
 			// );
-        }
+		}
 
 		// foreach ( $pending_orders_query->get() as $wc_order_model ) {
-		// 	try {
-		// 		Authorise_Tamara_Order_If_Possible_Job::dispatchSync(
-		// 			[
-		// 				'wc_order_id' => $wc_order_model->id,
-		// 			]
-		// 		);
+		//  try {
+		//      Authorise_Tamara_Order_If_Possible_Job::dispatchSync(
+		//          [
+		//              'wc_order_id' => $wc_order_model->id,
+		//          ]
+		//      );
 		// 	// phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedCatch
-		// 	} catch ( Exception $e ) {
-		// 	}
+		//  } catch ( Exception $e ) {
+		//  }
 		// }
 	}
 }
