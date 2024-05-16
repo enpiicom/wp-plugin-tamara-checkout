2df06aea9c43efee3d762f40dc79755ba48310e0
Trac Nguyen <npbtrac@yahoo.com>
2024-05-16 23:25:21 +0700

    Fix translation

--- src/App/Support/Traits/Tamara_Trans_Trait.php
+++ src/App/Support/Traits/Tamara_Trans_Trait.php
@@ -6,18 +6,18 @@ namespace Tamara_Checkout\App\Support\Traits;
 
 trait Tamara_Trans_Trait {
 	// phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
-	protected function __( $untranslated_text ) {
+	public function __( $untranslated_text ) {
 		return __( $untranslated_text, 'tamara' );
 	}
 
 	// phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
-	protected function _x( $untranslated_text, $context ): string {
+	public function _x( $untranslated_text, $context ): string {
 		// phpcs:ignore WordPress.WP.I18n.NonSingularStringLiteralText, WordPress.WP.I18n.NonSingularStringLiteralContext, WordPress.WP.I18n.NonSingularStringLiteralDomain
 		return _x( $untranslated_text, $context, 'tamara' );
 	}
 
 	// phpcs:ignore PSR2.Methods.MethodDeclaration.Underscore
-	protected function _n_noop( string $singular, string $plural ): array {
+	public function _n_noop( string $singular, string $plural ): array {
 		// phpcs:ignore WordPress.WP.I18n.NonSingularStringLiteralSingular, WordPress.WP.I18n.NonSingularStringLiteralPlural, WordPress.WP.I18n.NonSingularStringLiteralDomain
 		return _n_noop( $singular, $plural, 'tamara' );
 	}
